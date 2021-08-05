f18d91eab5433706059a2f41b503571be049dd16
nyon <>
2021-08-05 23:46:33 +0900

    add TIL20210805

--- TIL/TIL.playground/Contents.swift
+++ TIL/TIL.playground/Contents.swift
@@ -1,16 +1,37 @@
 import UIKit
 
-func maxRepeating(_ sequence: String, _ word: String) -> Int {
-    let max = sequence.count / word.count
-    guard max > 0 else { return 0 }
-    var result = 0
-    for count in 1...max where sequence.contains(String(repeating: word, count: count)) {
-        result = count
+struct TIL20210804 {
+    func maxRepeating(_ sequence: String, _ word: String) -> Int {
+        let max = sequence.count / word.count
+        guard max > 0 else { return 0 }
+        var result = 0
+        for count in 1...max where sequence.contains(String(repeating: word, count: count)) {
+            result = count
+        }
+        return result
     }
-    return result
+    
+    init() {
+        maxRepeating("ababccab", "ab")
+        maxRepeating("ababccab", "cb")
+    }
 }
 
-maxRepeating("ababccab", "ab")
-maxRepeating("ababccab", "cb")
+TIL20210804()
 
+struct TIL20210805 {
+    func execute(text: String) -> Result<Int, Error> {
+        guard let index = Int(text) else {
+            return .failure(NSError(domain: "xxx", code: -4000, userInfo: nil))
+        }
+        return .success(index)
+    }
+    
+    init() {
+        execute(text: "abc10")
+        execute(text: "1004")
+    }
+}
+
+TIL20210805()
 
